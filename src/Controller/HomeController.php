a814d4c93b07289541fcadc9251236a819682bee
Matheus Araujo de Melo <d3rr3tido@gmail.com>
2024-08-21 11:10:27 -0300

    commit da model com cakephp

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 
 namespace App\Controller;
-use Cake\Datasource\ConnectionManager;
 use Cake\ORM\TableRegistry;
 
 /**
@@ -14,12 +13,28 @@ class HomeController extends AppController
     public function index()
     {  /* 
             como renderizar uma view?
-            views ficam em templates/NomeController/nomeFuncao
+            views ficam em templates/NomeController/index
             layout fica em templates/layout/nomeLayout
         */
 
         $usersTable = TableRegistry::getTableLocator()->get('Users');
-        $users = $usersTable->find()->all();
+        //$users = $usersTable->find()->all();
+        $users = $usersTable->find('all', ['limit' => 5, 'order' => 'id desc'])->where(['id >' => 5]);
+
+        // COMO FAZER O CADASTRO DE UM DADO NA TABELA COM ENTITY
+        // Se eu passar o id vira um update
+        $userEntity = $usersTable->newEmptyEntity();
+        // $userEntity->id = 21;
+        $userEntity->firstName = 'Matheus Empregado receba';
+        $userEntity->lastName = 'Araujo de Melo';
+        $userEntity->email = 'email@email';
+        $userEntity->password = password_hash('123', PASSWORD_DEFAULT);
+
+        $saved = $usersTable->save($userEntity);
+
+        var_dump($saved);
+        die();
+
         $name = 'Alexandre';
         $age = 41;
 
